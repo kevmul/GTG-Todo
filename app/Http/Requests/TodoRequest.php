5520c2c8db5a29f3a486c0ef662f7fef8d14faf1
Kevin Mulholland <kmulholland123@gmail.com>
2022-11-14 21:55:46 -0500

    Creating our Todos

---
+++ app/Http/Requests/TodoRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TodoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|min:3|max:100'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'The title is required.',
+            'title.max' => 'The title cannot exceed 100 characters.'
+        ];
+    }
+}
--- tests/Feature/TodoTest.php
+++ tests/Feature/TodoTest.php
@@ -2,21 +2,85 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Todo;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TodoTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function a_todo_can_be_created()
+    {
+        $response = $this->post('/todo', [
+            'title' => 'My First TODO!'
+        ]);
+
+        $this->assertEquals('My First TODO!', $response['title']);
+    }
+
+    /** @test */
+    public function two_todos_can_be_created()
+    {
+        $this->post('/todo', [
+            'title' => 'My First TODO!'
+        ]);
+        $this->post('/todo', [
+            'title' => 'My Second Todo!'
+        ]);
+
+        tap(Todo::orderBy('id', 'desc')->first(), function ($todo) {
+            $this->assertEquals('My Second Todo!', $todo->title);
+        });
+    }
+
+    /** @test */
+    public function a_todo_can_be_completed()
+    {
+        Todo::factory()->create(['title' => 'My First Todo!']);
+
+        $response = $this->patch('todo/1/mark/completed');
+        $response->assertOk();
+
+        tap(Todo::first(), function ($todo) {
+            $this->assertEquals('My First Todo!', $todo->title);
+            $this->assertTrue($todo->isCompleted, 'isCompleted is not True.');
+        });
+    }
+
+    /**
+     * provider
+     */
+    public function dataProviderForTitleValidation()
+    {
+        return [
+            // field, value, should_succeed, error_message
+            ['title', 'My First TODO!', true, 'Todo with title should succeed.'],
+            ['title', '', false, 'The title is required.'],
+            ['title', 'ab', false, 'The title must be at least 3 characters.'],
+            ['title', implode('', array_fill(0, 100, 'a')), true, 'Title should allow for 100 chars.'],
+            ['title', implode('', array_fill(0, 101, 'a')), false, 'The title cannot exceed 100 characters.'],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider dataProviderForTitleValidation
+     */
+    public function it_can_validate_a_todo($field, $value, $should_succeed, $error_message)
+    {
+        $response = $this->post('/todo', [
+            $field => $value
+        ]);
+
+        if ($should_succeed) {
+            $response->assertStatus(201);
+        } else {
+            $response->assertStatus(302);
+            $response->assertSessionHasErrors($field);
+            $this->assertEquals($error_message, session('errors')->get($field)[0]);
+        }
     }
 }
