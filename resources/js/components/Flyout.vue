9fcaf479a12e2fb19486059b9832fda0d66f660f
Kevin Mulholland <kmulholland123@gmail.com>
2023-07-31 14:10:23 -0400

    [ep.16] eslint and prettier

--- resources/js/components/Flyout.vue
+++ resources/js/components/Flyout.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-
 const props = defineProps<{
     isOpen: boolean;
 }>();
 
 const emit = defineEmits<{
-    (e: "update:is-open", boolean): void;
+    (e: 'update:is-open', boolean): void;
 }>();
 </script>
 
@@ -19,7 +17,8 @@ const emit = defineEmits<{
     />
     <div
         v-show="props.isOpen"
-        class="absolute z-10 top-full rounded py-1 min-w-[120px] bg-white shadow-2xl border border-gray-300">
-            <slot/>
-        </div>
+        class="absolute z-10 top-full rounded py-1 min-w-[120px] bg-white shadow-2xl border border-gray-300"
+    >
+        <slot />
+    </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.ts"],
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
             refresh: true,
         }),
         vue({
@@ -13,10 +14,15 @@ export default defineConfig({
             includeAbsolute: false,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
+        },
+    },
     test: {
         globals: true,
-        exclude: ["node_modules"],
-        include: ["./resources/js/**/__tests__/*.spec.ts"],
-        environment: "jsdom",
+        exclude: ['node_modules'],
+        include: ['./resources/js/**/__tests__/*.spec.ts'],
+        environment: 'jsdom',
     },
 });
